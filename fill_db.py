cf7c3e9dbd9fc2a82751bc634dbab78bc0e131b7
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-05-24 10:27:53 -0400

    Dropped initials

--- fill_db.py
+++ fill_db.py
@@ -35,12 +35,10 @@ def fill(candidates_per_group):
         Position(name="Chef %s" % c, amount_of_electees="1", weight="-5").save()
         Position(name="Adjoint %s" % c, amount_of_electees="2", weight="0").save()
     first_names = ["Pierre", "Jean", "Jeanne", "Janice"]
-    initials = ["A.", "D.", "G.", "F."]
     last_names = ["LaFureur", "Lamontagne", "LaRoche", "Le Sorbier"]
     for i in range(candidates_per_group):
         for p in Position.objects.all():
             Candidate(first_name=random.choice(first_names), \
-                initial=random.choice(initials),\
                 last_name=random.choice(last_names),\
                 votes=random.randint(1,150),
                 position=p).save()
